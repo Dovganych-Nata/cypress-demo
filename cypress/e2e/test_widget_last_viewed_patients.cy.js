0703fe5ea84aaa6b2edadb2a55e45361b35c74be
Dovganych-Nata <dovganych.n@gmail.com>
2025-12-15 10:08:04 +0100

    add test widget structure

--- cypress/e2e/test_widget_last_viewed_patients.cy.js
+++ cypress/e2e/test_widget_last_viewed_patients.cy.js
@@ -1,3 +1,4 @@
+
 describe("Authenticated tests", () => {
   beforeEach(() => {
     cy.login();
@@ -8,16 +9,64 @@ describe("Authenticated tests", () => {
     cy.intercept('GET', '/fhir/rest/v1/fhir/Patient?identifier=*').as('getPatients');
 
     cy.wait('@getPatients', { timeout: 15000 }).then((interception) => {
-      // --- 🚨 Check 1: Verify the status code first 🚨 ---
-      expect(interception.response.statusCode).to.eq(200, 'Expected status code 200 for successful patient fetch.');
+      expect(interception.response.statusCode).to.eq(200);
 
       const responseBody = interception.response.body;
 
       const lastViewedPatientsCount = responseBody.total;
       cy.get('.widget-title').contains(` Last Viewed Patients (${lastViewedPatientsCount})`).should("be.visible");
+
+
+
+      const patients = interception.response.body.entry;
+
+
+      const formatDate = (dateString) => {      
+        const date = new Date(dateString);
+      
+        const months = [
+          'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+        ];
+      
+        const day = String(date.getUTCDate()).padStart(2, '0');
+        const month = months[date.getUTCMonth()];
+        const year = date.getUTCFullYear();
+      
+        return `${day}-${month}-${year}`;
+      };
+      
+
+      const result = patients.map(patient => {
+        return {
+            lastName: patient.resource.name
+             .filter(nameObj => nameObj.family)
+             .map(nameObj => nameObj.family)[0],
+          
+            firstName: patient.resource.name
+              .filter(nameObj => Array.isArray(nameObj.given))
+              .map(nameObj => nameObj.given[0])[0],
+            
+            birthDate: formatDate(patient.resource.birthDate),
+
+            mrnValue: patient.resource.identifier
+                .filter(mrn => mrn.system === "MRN")
+                .map(mrn => mrn.value)[0]
+        };
     });
 
+      cy.writeFile('cypress/fixtures/patients.json', result);
+      console.log(result); 
 
+      result.forEach(patient => {
+        cy.contains(`${patient.lastName}, ${patient.firstName}`)
+          .closest('.demographics-section')      
+          .should('contain', `${patient.lastName}`)
+          .should('contain', `${patient.firstName}`)
+          .should('contain', `${patient.birthDate}`)
+          .should('contain', `${patient.mrnValue}`)
+      });
+    });
 
   });
 });
